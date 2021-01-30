566619265ee92d44c6e363bcd97272f797523df4
AP <past0073@umn.edu>
2021-01-30 13:07:51 -0600

    Added audio element and source element that pulls desired song preview and appends to page. Created arrays of songs that correspond to different weather conditions.

--- coolio.js
+++ coolio.js
@@ -1,7 +1,37 @@
-$("#btn").on("click", function(e) {
+//From the weather API
+// cloudCoverage = response.data[0].clouds
+
+// if (response.data[0].clouds < 25) {
+//     query = sunnySongs[0];
+// }
+// else if (response.data[0].clouds > 25 && resposne.data[0].clouds < 50) {
+//     query = partsunnySongs[0];
+// }
+// else if (response.data[0].clouds > 50 && response.data[0].clouds < 75) {
+//     query = partcloudySongs[0];
+// } 
+// else {
+//     query = cloudySongs[0];
+// }
+
+//Song arrays for each type of weather condition:
+sunnySongs = ["Walking on Sunshine", "Best Day of My Life", "Once in a While Timeflies", "Juice", "Shut Up and Dance"];
+partsunnySongs = ["Peace Train", "Here Comes the Sun Beatles", "Clouds Fin", "Wouldn't it Be Nice", "I Wanna Dance with Somebody"];
+partcloudySongs = ["Landslide", "Hey ya!", "Can't Fight this Feeling", "Mr Blue Sky"];
+cloudySongs = ["Rainbow", "Cloudy Simon", "Pink Bullets", "A Plain Morning", "Yellow"]
+rainySongs = [""];
+snowySongs = [""];
+stormySongs = [""];
+
+//"One" vs on click prevents multiple instances of music playing over each other
+$("#testBtn").one("click", function(e) {
     e.preventDefault();
-query = $("#songInput").val().trim();
+
+//Using one song for now until we have the weather API to pull from 
+query = sunnySongs[0];
+
 console.log(query);
+
 const settings = {
         "async": true,
             "crossDomain": true,
@@ -15,35 +45,20 @@ const settings = {
 
     $.ajax(settings).done(function (response) {
 
-        //First search result
-        var newEl = $("<source>");
-        newEl.attr("src", response.data[0].preview);
-        newEl.attr("type", "audio/mpeg");
-        $("#song").prepend(newEl);
+        console.log(response);
 
-        //Second search result
-        var newEl = $("<source>");
-        newEl.attr("src", response.data[1].preview);
-        newEl.attr("type", "audio/mpeg");
-        $("#song").prepend(newEl);
-   
-        //Third search result
-        var newEl = $("<source>");
-        newEl.attr("src", response.data[2].preview);
-        newEl.attr("type", "audio/mpeg");
-        $("#song").prepend(newEl);
-             
-        //Fourth search result
-        var newEl = $("<source>");
-        newEl.attr("src", response.data[2].preview);
-        newEl.attr("type", "audio/mpeg");
-        $("#song").prepend(newEl);
+        //Create an audio div with controls to append song information to
+        audioEl = $("<audio>");
+        audioEl.attr("id", "song");
+        audioEl.attr("autoplay", true);
+        audioEl.attr("controls", true);
+        $("#music-display").prepend(audioEl);
 
-        //Fifth search result
-        var newEl = $("<source>");
-        newEl.attr("src", response.data[2].preview);
-        newEl.attr("type", "audio/mpeg");
-        $("#song").prepend(newEl);
+        //Append song to audio player div
+        var songEl = $("<source>");
+        songEl.attr("src", response.data[0].preview);
+        songEl.attr("type", "audio/mpeg");
+        audioEl.prepend(songEl);
 
         });
 });
