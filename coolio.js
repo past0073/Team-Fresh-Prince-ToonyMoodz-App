064fc534f794daafd0509f59b55ad04eedccfa42
AP <past0073@umn.edu>
2021-01-31 10:32:35 -0600

    Brought Weatherbit and Deezer APIs together so that Deezer can grab weather information to select which song array is appropriate. Changed song titles to track numbers.

--- coolio.js
+++ coolio.js
@@ -1,44 +1,68 @@
-//From the weather API
-// cloudCoverage = response.data[0].clouds
+$(".search-icon").on("click", function(e) {
+    e.preventDefault(); 
+    var city = $(".search-bar").val().trim();
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://weatherbit-v1-mashape.p.rapidapi.com/current?city=" + city + "&units=I&include=minutely&key=f9b72c322157431f92010fd9c3d81acd",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "a92d1921e7msh73d333a3f631533p1bcc10jsn76334283dba9",
+            "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com"
+        }
+    };
+    console.log(settings.url);
 
-// if (response.data[0].clouds < 25) {
-//     query = sunnySongs[0];
-// }
-// else if (response.data[0].clouds > 25 && resposne.data[0].clouds < 50) {
-//     query = partsunnySongs[0];
-// }
-// else if (response.data[0].clouds > 50 && response.data[0].clouds < 75) {
-//     query = partcloudySongs[0];
-// } 
-// else {
-//     query = cloudySongs[0];
-// }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        console.log(response.data[0].temp);
+        var icon = "https://www.weatherbit.io/static/img/icons/" + response.data[0].weather.icon + ".png";
+        var newIcon = $("<img>");
+        newIcon.attr("src", icon);
+        $(".title-cnt").append(newIcon);
 
-//Song arrays for each type of weather condition:
-sunnySongs = ["Walking on Sunshine", "Best Day of My Life", "Once in a While Timeflies", "Juice", "Shut Up and Dance"];
-partsunnySongs = ["Peace Train", "Here Comes the Sun Beatles", "Clouds Fin", "Wouldn't it Be Nice", "I Wanna Dance with Somebody"];
-partcloudySongs = ["Landslide", "Hey ya!", "Can't Fight this Feeling", "Mr Blue Sky"];
-cloudySongs = ["Rainbow", "Cloudy Simon", "Pink Bullets", "A Plain Morning", "Yellow"]
-rainySongs = [""];
-snowySongs = [""];
-stormySongs = [""];
+        //Song arrays for each type of weather condition:
+        sunnySongs = ["3166724", "1175777", "75711295", "666286232", "90326361"];
+        partsunnySongs = ["1094034052", "828216172", "1040347", "116348464", "1106540662", "3091978"];
+        partcloudySongs = ["3599559", "9956008", "65707299", "581533", "144469614"];
+        cloudySongs = ["482643502", "2607674", "1101367912", "121876594", "75623960", "637766082", "3128096"]
+        rainySongs = ["6069361", "1169683", "2794654", "705016142"];
+        snowySongs = ["3659065", "111624866", "75624084", "576178942"];
+        stormySongs = ["65445466", "1205382872", "92720102"];
 
-//"One" vs on click prevents multiple instances of music playing over each other
-$("#testBtn").one("click", function(e) {
-    e.preventDefault();
+        cloudCoverage = response.data[0].clouds
+        console.log(cloudCoverage);
+        
+        if (response.data[0].clouds < 25) {
+            query = sunnySongs[0];
+        }
+        else if (response.data[0].clouds > 25 && response.data[0].clouds < 50) {
+            query = partsunnySongs[0];
+        }
+        else if (response.data[0].clouds > 50 && response.data[0].clouds < 75) {
+            query = partcloudySongs[0];
+        } 
+        else {
+            query = cloudySongs[0];
+        }
 
-//Using one song for now until we have the weather API to pull from 
-query = sunnySongs[0];
+        runDeezerAPI();
+    });
+});
 
-console.log(query);
+function runDeezerAPI() {
+
+    event.preventDefault();
+    $("#music-display").empty();
+    console.log(query);
 
 const settings = {
         "async": true,
             "crossDomain": true,
-            "url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + query,
+            "url": "https://deezerdevs-deezer.p.rapidapi.com/track/" + query,
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "a92d1921e7msh73d333a3f631533p1bcc10jsn76334283dba9",
+                "x-rapidapi-key": "3a0b24119bmsh146e1af8ec6ab63p1333d4jsn7c3220356a72",
                 "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
             }
         }
@@ -56,9 +80,9 @@ const settings = {
 
         //Append song to audio player div
         var songEl = $("<source>");
-        songEl.attr("src", response.data[0].preview);
+        songEl.attr("src", response.preview);
         songEl.attr("type", "audio/mpeg");
         audioEl.prepend(songEl);
 
         });
-});
+};
