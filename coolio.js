c41ca2576800ee0c7b0dc7f2b4a8da73aac29d8b
Frank Merchlewitz <jlimhb@gmail.com>
2021-01-31 18:13:41 -0600

    updated api key

--- coolio.js
+++ coolio.js
@@ -5,12 +5,8 @@ $(".search-icon").on("click", function(e) {
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://weatherbit-v1-mashape.p.rapidapi.com/current?city=" + city + "&units=I&include=minutely&key=f9b72c322157431f92010fd9c3d81acd",
+        "url": "https://api.weatherbit.io/v2.0/current?city=" + city + "&units=I&include=minutely&key=f9b72c322157431f92010fd9c3d81acd",
         "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "a92d1921e7msh73d333a3f631533p1bcc10jsn76334283dba9",
-            "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com"
-        }
     };
     console.log(settings.url);
 
